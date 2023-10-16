7b69af6bc8e553811718105faae9c61830554a99
Justin Hamerly <suvimountain@icloud.com>
2023-10-16 14:57:56 -0700

    refactoring for types

--- client/src/components/CardDisplay/CardDisplay.tsx
+++ client/src/components/CardDisplay/CardDisplay.tsx
@@ -1,29 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import styles from './carddisplay.module.css';
+import { Paper } from '@mui/material';
 import PCard from './PCard/PCard';
 import data from '../../assets/data/cards.json';
-import { Paper } from '@mui/material';
 import FilterPanel from './FilterPanel/FilterPanel';
 
-
 interface CardDisplayProps { }
+
+const filterCardsByType = (selectedCardType: string) => {
+  if (selectedCardType === 'all') {
+    return data;
+  } else {
+    return data.filter(card => card.card_type === selectedCardType);
+  }
+};
+
+const mapCardsToPCard = (cardData: any[]) => {
+  return cardData.map(card => (
+    <PCard
+      imgPath={`/cardimg${card.img_path}`}
+      desc={card.name}
+      key={`${card.set}${card.set_num}`}
+      id={`${card.set}${card.set_num}`}
+    />
+  ));
+};
 
 const CardDisplay: React.FC<CardDisplayProps> = () => {
   const [cardDisplay, setCardDisplay] = useState(data);
   const [selectedCardType, setSelectedCardType] = useState('all');
 
   useEffect(() => {
-    if(selectedCardType === 'all'){
-      setCardDisplay(data);
-    }else{
-      const filteredData = data.filter(card => card.card_type === selectedCardType);
-      setCardDisplay(filteredData);
-    }
+
+    setCardDisplay(() => filterCardsByType(selectedCardType))
   }, [selectedCardType])
 
-  const cards = cardDisplay.map(cardData => {
-    return <PCard imgPath={'/cardimg' + cardData.img_path} desc={cardData.name} key={cardData.set + cardData.set_num} id={cardData.set + cardData.set_num} />
-  })
+  const cards = mapCardsToPCard(cardDisplay)
 
   return (
     <Paper className={styles.carddisplay} elevation={3}>
