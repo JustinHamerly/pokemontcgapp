f31b41f01ce43dc1d49f138f61e07672510afdb8
Justin Hamerly <suvimountain@icloud.com>
2023-10-16 15:23:45 -0700

    filter by pokemon type implementation

--- client/src/components/CardDisplay/CardDisplay.tsx
+++ client/src/components/CardDisplay/CardDisplay.tsx
@@ -15,6 +15,15 @@ const filterCardsByType = (selectedCardType: string) => {
   }
 };
 
+const filterCardsByPokemonType = (allowedTypes: string[]) => {
+  if (allowedTypes.length === 0) {
+    return data;
+  } else {
+    const pokemonList = filterCardsByType('pokemon');
+    return pokemonList.filter(card => allowedTypes.includes(card.type));
+  }
+}
+
 const mapCardsToPCard = (cardData: any[]) => {
   return cardData.map(card => (
     <PCard
@@ -29,17 +38,27 @@ const mapCardsToPCard = (cardData: any[]) => {
 const CardDisplay: React.FC<CardDisplayProps> = () => {
   const [cardDisplay, setCardDisplay] = useState(data);
   const [selectedCardType, setSelectedCardType] = useState('all');
+  const [selectedPokemonTypes, setSelectedPokemonTypes] = useState<string[]>([])
+
 
   useEffect(() => {
-
     setCardDisplay(() => filterCardsByType(selectedCardType))
   }, [selectedCardType])
+
+  useEffect(() => {
+    setCardDisplay(() => filterCardsByPokemonType(selectedPokemonTypes))
+  }, [selectedPokemonTypes])
 
   const cards = mapCardsToPCard(cardDisplay)
 
   return (
     <Paper className={styles.carddisplay} elevation={3}>
-      <FilterPanel selectedCardType={selectedCardType} setSelectedCardType={setSelectedCardType} />
+      <FilterPanel 
+        selectedCardType={selectedCardType} 
+        setSelectedCardType={setSelectedCardType} 
+        selectedPokemonTypes={selectedPokemonTypes} 
+        setSelectedPokemonTypes={setSelectedPokemonTypes} 
+      />
       <Paper className={styles.cards}>
         {cards}
       </Paper>
--- client/src/components/CardDisplay/FilterPanel/FilterPanel.tsx
+++ client/src/components/CardDisplay/FilterPanel/FilterPanel.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, Typography, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, FormControl, FormControlLabel, Radio, RadioGroup, Checkbox } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
 
 interface FilterPanelProps {
   setSelectedCardType: Function;
   selectedCardType: string;
+  setSelectedPokemonTypes: Function;
+  selectedPokemonTypes: string[];
 };
 
-const FilterPanel: React.FC<FilterPanelProps> = ({ selectedCardType, setSelectedCardType }) => {
+const FilterPanel: React.FC<FilterPanelProps> = ({ selectedCardType, selectedPokemonTypes, setSelectedCardType, setSelectedPokemonTypes }) => {
   
   const handleCardTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedCardType(event.target.value);
+  };
+
+  const handlePokemonTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const pokemonType = event.target.value;
+    setSelectedPokemonTypes((prevSelectedTypes: string[]) => {
+      if (prevSelectedTypes.includes(pokemonType)) {
+        return prevSelectedTypes.filter(type => type !== pokemonType);
+      } else {
+        return [...prevSelectedTypes, pokemonType];
+      }
+    });
   };
 
   return (
@@ -19,7 +32,6 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ selectedCardType, setSelected
         <Typography>FILTER</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Typography>Filters Here</Typography>
         <FormControl component="fieldset">
           <RadioGroup
             aria-label="cardType"
@@ -33,6 +45,76 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ selectedCardType, setSelected
             <FormControlLabel value="energy" control={<Radio />} label="Energy" />
           </RadioGroup>
         </FormControl>
+        {(selectedCardType === 'pokemon' || selectedCardType === 'all') && (
+          <FormControl component="fieldset">
+            <Typography variant="subtitle1">Pokemon Types:</Typography>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Grass"
+              value="grass"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('grass')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Fire"
+              value="fire"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('fire')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Water"
+              value="water"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('water')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Lightning"
+              value="lightning"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('lightning')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Psychic"
+              value="psychic"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('psychic')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Fighting"
+              value="fighting"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('fighting')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Darkness"
+              value="darkness"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('darkness')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Steel"
+              value="steel"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('steel')}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Colorless"
+              value="colorless"
+              onChange={handlePokemonTypeChange}
+              checked={selectedPokemonTypes.includes('colorless')}
+            />
+            
+            
+          </FormControl>
+        )}
       </AccordionDetails>
     </Accordion>
   )
