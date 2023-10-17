a53cf8f620dd226c00dc651a2653c56098f2175a
Justin Hamerly <suvimountain@icloud.com>
2023-10-17 15:34:20 -0700

    filter for move types implemented

--- client/src/components/CardDisplay/CardDisplay.tsx
+++ client/src/components/CardDisplay/CardDisplay.tsx
@@ -2,32 +2,52 @@ import React, { useEffect, useState } from 'react';
 import styles from './carddisplay.module.css';
 import { Paper, cardActionAreaClasses } from '@mui/material';
 import PCard from './PCard/PCard';
-import data from '../../assets/data/cards.json';
 import FilterPanel from './FilterPanel/FilterPanel';
+import axios from 'axios';
 
 interface CardDisplayProps { }
+
+let fullSet: any[] = [];
+
+const getCardsFromDB = async () => {
+  try {
+    const cardResults = await axios.get('http://localhost:3001/all');
+    fullSet = cardResults.data;
+  } catch (error) {
+    console.error(error);
+    fullSet = [];
+  }
+}
 
 const filterCardsByGenerics = (selectedCardType: string, rarity: string[]) => {
   const filterByCardType = selectedCardType !== 'all';
   const filterByRarity = rarity.length !== 0;
 
-  return data.filter(card => {
-    const matchesCardType = !filterByCardType || card.card_type === selectedCardType;
+  return fullSet.filter(card => {
+    const matchesCardType = !filterByCardType || card.cardType === selectedCardType;
     const matchesRarity = !filterByRarity || rarity.includes(card.rarity);
     return matchesCardType && matchesRarity;
   });
 };
 
-const filterCardsByPokemon = (list: card[], allowedTypes: string[], allowedStages: string[], ex: boolean, tera: boolean, rarity: string[], hasAbility: boolean) => {
-  if (allowedTypes.length === 0 && allowedStages.length === 0 && !ex && !tera && !hasAbility) {
+const filterCardsByPokemon = (list: any[], allowedTypes: string[], allowedStages: string[], ex: boolean, tera: boolean, hasAbility: boolean, health: number[], moveCosts: string[]) => {
+  if (allowedTypes.length === 0 && allowedStages.length === 0 && !ex && !tera && !hasAbility && health[0] == 0 && health[1] == 400 && moveCosts.length === 0) {
     return list;
+  }
+
+  const checkMoveCosts = (costs: string[], allowedCosts: string[]): boolean => {
+    if (costs){
+      return costs.some(cost => allowedCosts.includes(cost));
+    }
+    return false;
   }
 
   const filteredPokemonList = list.filter(card => {
     const isTypeAllowed = allowedTypes.length === 0 || allowedTypes.includes(card.type as string);
     const isStageAllowed = allowedStages.length === 0 || allowedStages.includes(card.stage as string);
+    const isMoveCostAllowed = moveCosts.length === 0 || checkMoveCosts(card.moveCosts, moveCosts);
 
-    return (!tera || card.tera) && (!ex || card.ex) && (!hasAbility || card.ability) && isTypeAllowed && isStageAllowed;
+    return (!tera || card.tera) && (!ex || card.ex) && (!hasAbility || card.ability) && isTypeAllowed && isStageAllowed && card.hp <= health[1] && card.hp >= health[0] && isMoveCostAllowed;
   });
 
   return filteredPokemonList;
@@ -37,16 +57,18 @@ const filterCardsByPokemon = (list: card[], allowedTypes: string[], allowedStage
 const mapCardsToPCard = (cardData: any[]) => {
   return cardData.map(card => (
     <PCard
-      imgPath={`/cardimg${card.img_path}`}
+      imgPath={`/cardimg${card.imgPath}`}
       desc={card.name}
-      key={`${card.set}${card.set_num}`}
-      id={`${card.set}${card.set_num}`}
+      key={card.id}
+      id={card.id}
     />
   ));
 };
 
+
+
 const CardDisplay: React.FC<CardDisplayProps> = () => {
-  const [cardDisplay, setCardDisplay] = useState(data);
+  const [cardDisplay, setCardDisplay] = useState<any[]>([]);
   const [cardType, setCardType] = useState('all');
   const [rarity, setRarity] = useState<string[]>([]);
   const [pokemonTypes, setPokemonTypes] = useState<string[]>([])
@@ -54,12 +76,18 @@ const CardDisplay: React.FC<CardDisplayProps> = () => {
   const [pokemonEx, setPokemonEx] = useState<boolean>(false);
   const [pokemonTera, setPokemonTera] = useState<boolean>(false);
   const [hasAbility, setHasAbility] = useState<boolean>(false);
+  const [health, setHealth] = useState([0,400]);
+  const [moveCosts, setMoveCosts] = useState<string[]>([]);
+
+  useEffect(() => {
+    getCardsFromDB().then(() => setCardDisplay(fullSet));
+  }, []);
 
   useEffect(() => {
     const filterGenerics = filterCardsByGenerics(cardType, rarity);
-    const filteredPokemon = filterCardsByPokemon(filterGenerics, pokemonTypes, pokemonStages, pokemonEx, pokemonTera, rarity, hasAbility)
+    const filteredPokemon = filterCardsByPokemon(filterGenerics, pokemonTypes, pokemonStages, pokemonEx, pokemonTera, hasAbility, health, moveCosts)
     setCardDisplay(filteredPokemon)
-  }, [cardType, rarity, pokemonTypes, pokemonStages, pokemonEx, pokemonTera, hasAbility])
+  }, [cardType, rarity, pokemonTypes, pokemonStages, pokemonEx, pokemonTera, hasAbility, health, moveCosts])
 
   const cards = mapCardsToPCard(cardDisplay)
 
@@ -80,6 +108,10 @@ const CardDisplay: React.FC<CardDisplayProps> = () => {
         setRarity={setRarity}
         hasAbility={hasAbility}
         setHasAbility={setHasAbility}
+        health={health}
+        setHealth = {setHealth}
+        setMoveCosts = {setMoveCosts}
+        moveCosts = {moveCosts}
       />
       <Paper className={styles.cards}>
         {cards}
--- client/src/components/CardDisplay/FilterPanel/FilterPanel.tsx
+++ client/src/components/CardDisplay/FilterPanel/FilterPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, Typography, FormControl, FormControlLabel, Radio, RadioGroup, Checkbox, Switch } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, FormControl, FormControlLabel, Radio, RadioGroup, Checkbox, Switch, Slider } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
 
 interface FilterPanelProps {
@@ -17,33 +17,40 @@ interface FilterPanelProps {
   rarity: string[];
   setHasAbility: Function;
   hasAbility: boolean;
-
+  setHealth: Function;
+  health: number[];
+  setMoveCosts: Function;
+  moveCosts: string[];
 };
 
-const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, cardType, pokemonTypes, pokemonStages, setCardType, setPokemonTypes, setPokemonStages, pokemonTera, setPokemonTera, rarity, setRarity, setHasAbility, hasAbility }) => {
+const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, cardType, pokemonTypes, pokemonStages, setCardType, setPokemonTypes, setPokemonStages, pokemonTera, setPokemonTera, rarity, setRarity, setHasAbility, hasAbility, setHealth, health, moveCosts, setMoveCosts }) => {
 
-  const handleCardTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleCardTypeChange = (event: any) => {
+    console.log(event.target.value)
     const cardType = event.target.value;
-    if(cardType === 'pokemon'){
+    if (cardType === 'pokemon') {
 
-    }else if(cardType === 'trainer'){
+    } else if (cardType === 'trainer') {
       setPokemonTera(false);
       setPokemonEx(false);
       setPokemonStages([]);
       setPokemonTypes([]);
       setHasAbility(false);
-    }else if(cardType === 'energy'){
+      setHealth([0,400]);
+      
+    } else if (cardType === 'energy') {
       setRarity([]);
       setPokemonStages([]);
       setPokemonTypes([]);
       setPokemonTera(false);
-      setPokemonEx(false); 
+      setPokemonEx(false);
       setHasAbility(false);
+      setHealth([0,400]);
     }
     setCardType(cardType);
   };
 
-  const handlePokemonTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePokemonTypeChange = (event: any) => {
     const pokemonType = event.target.value;
     setPokemonTypes((prevTypes: string[]) => {
       if (prevTypes.includes(pokemonType)) {
@@ -54,7 +61,7 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, card
     });
   };
 
-  const handlePokemonStageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePokemonStageChange = (event: any) => {
     const pokemonStage = event.target.value;
     setPokemonStages((prevStages: string[]) => {
       if (prevStages.includes(pokemonStage)) {
@@ -65,25 +72,25 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, card
     });
   };
 
-  const handlePokemonExChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePokemonExChange = (event: any) => {
     setPokemonEx((prevExState: boolean) => {
       return !prevExState;
     })
   }
 
-  const handlePokemonTeraChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePokemonTeraChange = (event: any) => {
     setPokemonTera((prevExState: boolean) => {
       return !prevExState;
     })
   }
 
-  const handleHasAbilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleHasAbilityChange = (event: any) => {
     setHasAbility((prevAbilityState: boolean) => {
       return !prevAbilityState;
     })
   }
 
-  const handleRarityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleRarityChange = (event: any) => {
     const rarity = event.target.value;
     setRarity((prevRarity: string[]) => {
       if (prevRarity.includes(rarity)) {
@@ -94,6 +101,22 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, card
     })
 
   }
+
+  const handleHealthChange = (event: Event, newValue: number | number[]) => {
+    console.log(newValue)
+    setHealth(newValue as number[]);
+  }
+
+  const handleMoveCostChange = (event: any) => {
+    const cost = event.target.value;
+    setMoveCosts((prevTypes: string[]) => {
+      if (prevTypes.includes(cost)) {
+        return prevTypes.filter(type => type !== cost);
+      } else {
+        return [...prevTypes, cost];
+      }
+    });
+  };
 
   return (
     <Accordion>
@@ -118,42 +141,49 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, card
         <FormControl component="fieldset">
           <Typography>RARITY</Typography>
           <FormControlLabel
-            control={<Checkbox/>}
+            control={<Checkbox />}
             label="Common"
             value="common"
             onChange={handleRarityChange}
             checked={rarity.includes('common')}
           />
           <FormControlLabel
-            control={<Checkbox/>}
+            control={<Checkbox />}
             label="Uncommon"
             value="uncommon"
             onChange={handleRarityChange}
             checked={rarity.includes('uncommon')}
           />
           <FormControlLabel
-            control={<Checkbox/>}
+            control={<Checkbox />}
             label="Rare"
             value="rare"
             onChange={handleRarityChange}
             checked={rarity.includes('rare')}
           />
           <FormControlLabel
-            control={<Checkbox/>}
-            label="Ultra Rare"
-            value="ultra-rare"
+            control={<Checkbox />}
+            label="Ex Rare"
+            value="double-rare"
             onChange={handleRarityChange}
-            checked={rarity.includes('ultra-rare')}
+            checked={rarity.includes('double-rare')}
           />
           <FormControlLabel
-            control={<Checkbox/>}
+            control={<Checkbox />}
             label="Art Rare"
             value="a-rare"
             onChange={handleRarityChange}
             checked={rarity.includes('a-rare')}
           />
           <FormControlLabel
-            control={<Checkbox/>}
+            control={<Checkbox />}
+            label="Ultra Rare"
+            value="ultra-rare"
+            onChange={handleRarityChange}
+            checked={rarity.includes('ultra-rare')}
+          />
+          <FormControlLabel
+            control={<Checkbox />}
             label="Special Art Rare"
             value="sa-rare"
             onChange={handleRarityChange}
@@ -282,6 +312,77 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ pokemonEx, setPokemonEx, card
                 checked={hasAbility}
               />
             </FormControl>
+            <Typography>HEALTH:</Typography>
+            <Slider
+              value={health}
+              min={0}
+              max={400}
+              step={10}
+              onChange={handleHealthChange}
+              valueLabelDisplay="auto"
+              aria-labelledby="health-slider"
+            />
+            <FormControl component="fieldset">
+              <Typography variant="subtitle1">MoveCost</Typography>
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Grass"
+                value="grass"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('grass')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Fire"
+                value="fire"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('fire')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Water"
+                value="water"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('water')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Lightning"
+                value="lightning"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('lightning')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Psychic"
+                value="psychic"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('psychic')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Fighting"
+                value="fighting"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('fighting')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Darkness"
+                value="darkness"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('darkness')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Metal"
+                value="metal"
+                onChange={handleMoveCostChange}
+                checked={moveCosts.includes('metal')}
+              />
+
+            </FormControl>
+
           </>
         )}
       </AccordionDetails>
