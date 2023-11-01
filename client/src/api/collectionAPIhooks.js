4002c683c639769c5433b12e92c7b0692b9a2af3
Justin Hamerly <suvimountain@icloud.com>
2023-11-01 12:34:16 -0700

    moved fetch function to useEffect

--- client/src/api/collectionAPIhooks.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchAllCardData = async () => {
-  const {data} = await axios({method: 'get', baseURL: process.env.COLLECTIONSERVER, url: '/all'});
-  return data;
-}
--- client/src/context/CollectionContext.jsx
+++ client/src/context/CollectionContext.jsx
@@ -1,4 +1,5 @@
 import React, { useState, createContext, useEffect } from 'react';
+import axios from 'axios';
 
 export const CollectionContext = createContext();
 
@@ -6,7 +7,11 @@ const CollectionProvider = (props) => {
 
   const [collection, setCollection] = useState(null);
   useEffect(() => {
-    
+    const fetchAllCardData = async () => {
+      const {data} = await axios({method: 'get', baseURL: process.env.COLLECTIONSERVER, url: '/all'});
+      setCollection(data);
+    }
+    fetchAllCardData();
   }, []);
 
 
