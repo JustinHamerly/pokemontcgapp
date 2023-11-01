bf87df41054636a3785db1c239cf26b4e7c60f1a
Justin Hamerly <suvimountain@icloud.com>
2023-11-01 11:15:03 -0700

    moved top navigation bar to its own component

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import CardCollection from './CardCollection/CardCollection'
 import Account from './Account/Account'
 import Game from './Game/Game'
 import Inventory from './Inventory/Inventory'
-import { Button } from '@mui/material'
 
 export default function Main() {
-
-  const pages = ['', 'collection', 'game', 'inventory', 'account'];
-
   return (
-    <BrowserRouter>
-
-      {pages.map(page => (
-        <Button>
-          <Link to={"/"+page}>{page}</Link>
-        </Button>   
-      ))}
-      
-      <Routes>
-        <Route path="/" element={<h1>Landing Page</h1>}/>
-        <Route path="/collection" element={<CardCollection />}/>
-        <Route path="/game" element={<Game />}/>
-        <Route path="/inventory" element={<Inventory />}/>
-        <Route path="/account" element={<Account />}/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Landing Page</h1>} />
+      <Route path="/collection" element={<CardCollection />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/inventory" element={<Inventory />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
   )
 }
